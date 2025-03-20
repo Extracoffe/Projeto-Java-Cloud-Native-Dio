1f9e0f7c12aad367a837fc8739f51161d18657ab
André <andrerodartematos@gmail.com>
2025-03-20 15:45:12 -0300

    Testei métodos e concatenação

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,5 +1,16 @@
 public class MinhaClasse {
    public static void main (String [] args) {
-    System.out.print("Hello World");
+    String primeiroNome = "André";
+    String segundoNome = "Jorginho";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+
    }
+
+   public static String nomeCompleto(String primeiroNome, String segundoNome) {
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+   }
+
 }
